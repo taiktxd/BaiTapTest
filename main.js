4c3bd86a559282c5a5a60f642bfaeedf6d3251f4
taiktxd <taiktxd@gmail.com>
2023-12-23 10:54:30 +0700

    done

---
+++ main.js
@@ -0,0 +1,37 @@
+// const tinh =document.querySelector('#tinh1');
+// const ketQua1 = document.querySelector('#ketQua1')
+
+// const diemTrungBinh = () => {
+//     let toan = Number(document.querySelector('#diemToan').value)
+//     let ly = Number(document.querySelector('#diemLy').value)
+//     let hoa = Number(document.querySelector('#diemHoa').value)
+//     let diemTB = (toan + ly + hoa)/3;
+//     if(toan && ly && hoa){
+//     ketQua1.textContent = diemTB
+
+//     }else {
+//         alert('Vui lòng nhập đầy đủ thông tin ở lớp 1')
+//     }
+// }
+
+// tinh.addEventListener('click', diemTrungBinh)
+
+// const tinh2 = document.querySelector('#tinh2')
+// const ketQua2 = document.querySelector('#ketQua2')
+
+
+// const diemTrungBinh2 = () => {
+//     let van = Number(document.querySelector('#diemVan').value)
+//     let su = Number(document.querySelector('#diemSu').value)
+//     let dia = Number(document.querySelector('#diemDia').value)
+//     let anh = Number(document.querySelector('#anh').value)
+//     let diemTB2 = (van + su + dia + anh)/4;
+//     if(van && su && dia && anh){
+//         ketQua2.textContent = diemTB2
+//     }else {
+//             alert('Vui lòng nhập đầy đủ thông tin ở lớp 2')
+//     }
+// }
+
+// tinh2.addEventListener('click', diemTrungBinh2)
+
